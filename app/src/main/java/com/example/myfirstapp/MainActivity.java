06718b05767b613c49e6cc9fe982932f2ca677a4
VoltXd <47433478+VoltXd@users.noreply.github.com>
2023-03-19 15:30:46 +0100

    Dynamic layout

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,25 +1,36 @@
 package com.example.myfirstapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
-
-    private ImageView play;
+    private Button play;
+    private LinearLayout myDynamicLayout;
+    private ImageView imageChopper;
+    private MainActivity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+        // Set the main activity to be this activity
+        activity = this;
 
         // Get the android image
-        this.play = (ImageView) findViewById(R.id.play);
+        this.play = (Button) findViewById(R.id.play);
 
         // Set an OnClick event on the android image
         play.setOnClickListener(new View.OnClickListener()
@@ -37,5 +48,44 @@ public class MainActivity extends AppCompatActivity
                 finish();
             }
         });
+
+        // Dynamic Layout
+        this.myDynamicLayout = (LinearLayout) findViewById(R.id.myDynamicLayout);
+        this.imageChopper = (ImageView) findViewById(R.id.imageChopper);
+
+        imageChopper.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Chopper clicked ask for a new chopper
+                AlertDialog.Builder chopperPopup = new AlertDialog.Builder(activity);
+                chopperPopup.setTitle("Chopper");
+                chopperPopup.setMessage("Ajouter un Chopper ?");
+                chopperPopup.setNegativeButton("Non", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Toast.makeText(getApplicationContext(), "Vous rendez Chopper triste...", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                chopperPopup.setPositiveButton("Oui", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Toast.makeText(getApplicationContext(), "Ajout d'un Chopper", Toast.LENGTH_SHORT).show();
+
+                        Button button = new Button(activity);
+                        button.setText("Texte Bouton");
+                        myDynamicLayout.addView(button);
+                    }
+                });
+
+                chopperPopup.show();
+            }
+        });
+
     }
 }
