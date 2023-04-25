a85c813478f8e9c13f8eaa17896c3188f6a60345
VoltXd <47433478+VoltXd@users.noreply.github.com>
2023-04-25 12:01:12 +0200

    Update IfDef.java

--- app/src/main/java/com/example/myfirstapp/IfDef.java
+++ app/src/main/java/com/example/myfirstapp/IfDef.java
@@ -2,9 +2,9 @@ package com.example.myfirstapp;
 
 public class IfDef
 {
-    public static final boolean INITIALISATION_BLE = true;
-    public static final boolean SCAN_BLE = true;
-    public static final boolean GOTO_CONTROL_ACTIVITY = true;
-    public static final boolean GATT_CONNECT = true;
+    public static final boolean INITIALISATION_BLE = false;
+    public static final boolean SCAN_BLE = false;
+    public static final boolean GOTO_CONTROL_ACTIVITY = false;
+    public static final boolean GATT_CONNECT = false;
     public static final boolean DISCOVER_SERVICES = false;
 }
